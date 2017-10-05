c9239804e5585570e0130c9e562f0f651dd36600
BuildTools <unconfigured@null.spigotmc.org>
2017-10-04 21:31:32 -0400

    Basic splitting for alts in accounts.txt

--- AltsGUIBackend/Program.cs
+++ AltsGUIBackend/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AltsGUIBackend
@@ -10,7 +11,26 @@ namespace AltsGUIBackend
     {
         static void Main(string[] args)
         {
-            //test
+            string path = @"accounts.txt";
+            string accountInput = "0";
+            string[] accountLines = { };
+            if (File.Exists(path))
+            {
+                accountInput = File.ReadAllText(path);
+                accountLines = accountInput.Split('\n');
+            }
+            else
+            {
+                Console.WriteLine("Error! File was not found! Try again and relaunch application!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            for (int i = 0; i < accountLines.Length; i++)
+            {
+                Console.WriteLine(accountLines[i]);
+            }
+            Console.ReadLine();
+
         }
     }
 }
